87bf1f2eb0e7416f3ecb2195020f2490a5d9718b
yashbaghel451 <yashbaghel451@gmail.com>
2026-07-29 02:05:20 +0530

    fix profile image upload password hashing

--- Backend/src/model/User.model.js
+++ Backend/src/model/User.model.js
@@ -26,13 +26,15 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
-    console.log("Not Modified");
+    return next();
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
